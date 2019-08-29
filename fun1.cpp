6fe8fda1f7365b08bf9e50e326564fde9813c138
gaoyinheng <3088579946@qq.com>
2019-08-29 10:42:33 +0800

    fun1.cpp已提交

---
+++ fun1.cpp
@@ -0,0 +1,11 @@
+// fun1.cpp : Defines the entry point for the console application.
+//
+include <stdio.h>
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+	printf("Hello World!\n");
+	return 0;
+}
+
