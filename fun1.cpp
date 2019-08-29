e3581d76670cc03b31577f0ba665de75c91d47ad
gaoyinheng <3088579946@qq.com>
2019-08-29 13:38:43 +0800

    20190829-1338

--- fun1.cpp
+++ fun1.cpp
@@ -8,6 +8,8 @@ int main(int argc, char* argv[])
 	printf("Hello World!\n");
 	printf("Do this for test git.\n");
 	printf("Test for rollback.");
+	printf("2019-08-29 13:36-test.");
+	printf("2019-08-29 13:37-test.");
 	return 0;
 }
 
